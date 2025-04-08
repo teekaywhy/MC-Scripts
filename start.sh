cfe269a19717d89c0dcac94a5c1f5f0a4ccfd61b
teekaywhy <r.macmillan90@gmail.com>
2025-04-08 21:14:32 +0100

    asd

--- start.sh
+++ start.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-# Run a Minecraft server in a screen called Caves
-screen -dmS Caves
-screen -S Caves -p 0 -X stuff "java -Xmx6G -Xms6G -jar /home/asdf/fabric/server.jar nogui\n"
+# Run a Minecraft server in a tmux session called Caves
+
+# Change to the Minecraft server directory
+cd /home/asdf/fabric || {
+    echo "Failed to change to /home/asdf/fabric"
+    exit 1
+}
+
+# Kill existing tmux session if it exists
+tmux has-session -t Caves 2>/dev/null
+if [ $? -eq 0 ]; then
+    tmux kill-session -t Caves
+fi
+
+# Start the server in a new detached tmux session
+tmux new-session -d -s Caves "java -Xmx8G -Xms8G -jar /home/asdf/fabric/server.jar nogui"
